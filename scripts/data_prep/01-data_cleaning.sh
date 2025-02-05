fac2893eb1bb9590f7c136b94fd3629557a47de4
James Guo <guozy@stanford.edu>
2025-02-05 00:25:59 +0000

    Prepared data.

--- scripts/data_prep/01-data_cleaning.sh
+++ scripts/data_prep/01-data_cleaning.sh
@@ -5,8 +5,19 @@
 
 # This script renders all RMD files for data preparation and generates their HTML reports.
 
+exec > ./log/01-data_cleaning_log.txt 2>&1
+
+
+echo '@ start rendering 01a-metadata.Rmd...'
 Rscript -e "rmarkdown::render('01a-metadata.Rmd', output_dir = './html/')" > ./log/01a-metadata_log.txt 2>&1
+echo '@ 01a-metadata.Rmd rendered'
 
+echo '@ start rendering 01b-nmr_processing.Rmd...'
 Rscript -e "rmarkdown::render('01b-nmr_processing.Rmd', output_dir = './html/')" > ./log/01b-nmr_processing_log.txt 2>&1
+echo '@ 01b-nmr_processing.Rmd rendered'
 
+echo '@ start rendering 01c-nmr_metabolomics.Rmd...'
 Rscript -e "rmarkdown::render('01c-nmr_metabolomics.Rmd', output_dir = './html/')" > ./log/01c-nmr_metabolomics_log.txt 2>&1
+echo '@ 01c-nmr_metabolomics.Rmd rendered'
+
+echo '@ all 01 files rendered.'
